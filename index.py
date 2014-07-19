482680bec256b78811ab5c22f98aa4aa63f774d9
Matthew Gyurgyik <pyther@pyther.net>
2014-07-19 18:00:55 -0400

    formatting fixes

--- index.py
+++ index.py
@@ -13,32 +13,28 @@ app = Flask(__name__)
 app.config['PROPAGATE_EXCEPTIONS'] = True
 
 def format_pkg_name(pkg):
-    maxL = 24
+    max_length = 24
 
-    name,ver = pkg.split(' ')[:2]
-    ver,rel = ver.split('-')
+    name, ver = pkg.split(' ')[:2]
+    ver, rel = ver.split('-')
 
     full_name = '{0} {1}-{2}'.format(name, ver, rel)
 
-    if len(full_name) <= maxL:
+    if len(full_name) <= max_length:
         return (full_name, full_name)
 
     short_name = '{0} {1}'.format(name, ver)
 
-    if (len(short_name) <= maxL):
+    if len(short_name) <= max_length:
         return (full_name, short_name)
 
     short_name = name
 
-    if (len(short_name) <= maxL):
+    if len(short_name) <= max_length:
         return (full_name, short_name)
 
-    return (full_name, short_name[:maxL])
+    return (full_name, short_name[:max_length])
 
-
-@app.route('/feed/<feed>/<arch>')
-def fetch_feed2(feed, arch):
-    return arch
 
 @app.route('/feed/<name>')
 def fetch_feed(name):
@@ -55,7 +51,7 @@ def fetch_feed(name):
         abort(404)
 
     def write_feed(cache_file, feed):
-        data=(time.time(), feed)
+        data = (time.time(), feed)
         pickle.dump(data, open(cache_file, "wb"))
         return
 
@@ -79,7 +75,7 @@ def fetch_feed(name):
         if diff > expire_cache:
             feed = feedparser.parse(rss_feed, etag=cached_feed.etag)
             # feed has not been modified, update mtime so we don't check again
-            if d2.status == 304:
+            if feed.status == 304:
                 feed = cached_feed
                 # write updated mtime to pickle file
                 write_feed(cache_file, feed)
@@ -97,14 +93,14 @@ def fetch_feed(name):
         write_feed(cache_file, feed)
 
     if name == 'news':
-        items = [ (x.title, x.link) for x in feed.entries]
-        return render_template('news.html', news = items)
+        items = [(x.title, x.link) for x in feed.entries]
+        return render_template('news.html', news=items)
     elif 'pkgs' in name:
         pkgs = []
         for x in feed.entries[:MAX_PKGS]:
-            full,short = format_pkg_name(x.title)
+            full, short = format_pkg_name(x.title)
             pkgs.append({'name':full, 'short_name':short, 'cat':x.category, 'link':x.link, 'summary':x.summary})
-        return render_template('pkgs.html', pkgs = pkgs)
+        return render_template('pkgs.html', pkgs=pkgs)
 
     return 'None'
 
@@ -113,5 +109,5 @@ def index():
     return render_template("index.html")
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0',debug=True)
+    app.run(host='0.0.0.0', debug=True)
 
